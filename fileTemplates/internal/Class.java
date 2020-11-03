9356c1f996e812f92b1ad08cb82f39654d873d02
hspcadmin <hspcadmin@192.168.132.164>
2020-11-03 11:29:49 +0800

    IU-2019.2.4 <hspcadmin@WINDOWS-5LIDNJP Overwrite remote https://github.com/gaoih/ideaSetting.git to local

--- fileTemplates/internal/Class.java
+++
@@ -1,9 +0,0 @@
-#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
-#parse("File Header.java")
-/**
-*@description: ${description}
-*@author: gaoh
-*@create: ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}:${SECOND}
-**/
-public class ${NAME} {
-}
