4f356ff1085c89b0d7d238419b71e9c5ada49503
hspcadmin <hspcadmin@192.168.132.164>
2020-11-03 11:29:51 +0800

    IU-2019.2.4 <hspcadmin@WINDOWS-5LIDNJP Create baseRefactoring.xml, ide.general.xml, Default for Windows copy.xml, project.default.xml, laf.xml, Class.java, jdk.table.xml, Default.xml, path.macros.xml, find.xml, ui.lnf.xml, filetypes.xml, projectView.xml, keymap.xml, vcs.xml, editor.codeinsight.xml, colors.scheme.xml, toStringTemplates.xml, notifications.xml, editor.xml, mavenVersion.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+*@description: ${description}
+*@author: gaoh
+*@create: ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}:${SECOND}
+**/
+public class ${NAME} {
+}
